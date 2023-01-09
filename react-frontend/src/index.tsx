66dc3b55fc9b3ba1eddf149ac79a7074912819af
haliltokszz <toksozhalil@gmail.com>
2023-01-09 18:15:15 +0300

    nonessential

--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-            <Router>
-                <App />
-            </Router>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
